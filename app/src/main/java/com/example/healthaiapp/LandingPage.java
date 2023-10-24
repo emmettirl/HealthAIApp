26941305b3fe962e43be30b5ee2f008c7449f049
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-10-24 20:15:56 +0100

    - Updated Activity Page to welcome user by name - Updated LandingPage.Java to handle getting user names - Updates strings.xml to include localisation for activity_subscription_page.xml - Updated activity_subscription_page.xml with preliminary layout - Added header to activity_subscription_page.xml

--- app/src/main/java/com/example/healthaiapp/LandingPage.java
+++ app/src/main/java/com/example/healthaiapp/LandingPage.java
@@ -6,12 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.healthaiapp.data.User;
 
 public class LandingPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Intent thisIntent = getIntent();
+        if (thisIntent.hasExtra("loggedInUser")) {
+            User loggedInUser = (User) thisIntent.getSerializableExtra("loggedInUser");
+            assert loggedInUser != null;
+            String name = loggedInUser.getUsername();
+
+            TextView welcomeText = findViewById(R.id.LandingPageWelcomeText);
+            String greeting = getResources().getString(R.string.landing_greeting, name);
+            welcomeText.setText(greeting);
+        }
 
         //region Nav Buttons
         Button userProfileButton = findViewById(R.id.userProfileNavButton);
