651639fa3e9f34a6991c5f3b276286cc14fd52c1
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-10-24 17:49:08 +0100

    - Created activity_user_profile_page.xml - Created UserProfilePage.java - Create activity_subscription_page.xml
    - Added basic functionality for the nav buttons inside the landing page.
    - Refactored names of nav buttons
    - Recolored text in Landing Page
    - Adjusted image button sizes and alignment in the landing page

--- app/src/main/java/com/example/healthaiapp/LandingPage.java
+++ app/src/main/java/com/example/healthaiapp/LandingPage.java
@@ -2,7 +2,9 @@ package com.example.healthaiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Button userProfileButton = findViewById(R.id.userProfileNavButton);
+        Button AIPredictPLACEHOLDER = findViewById(R.id.FitnessNavButton);
+        Button FitnessPLACEHOLDER = findViewById(R.id.AIPredictNavButton);
+        userProfileButton.setOnClickListener(view -> {
+            Intent intent = new Intent(LandingPage.this, UserProfilePage.class);
+            startActivity(intent);
+        });
+
+
     }
 }
