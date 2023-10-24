85d61147595c6886e1777561251a6bac55ec3ec3
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-10-24 18:14:47 +0100

    Fixed issue with User Profile button crashing when navigating Landing -> User Profile page

--- app/src/main/java/com/example/healthaiapp/UserProfilePage.java
+++ app/src/main/java/com/example/healthaiapp/UserProfilePage.java
@@ -1,4 +1,14 @@
 package com.example.healthaiapp;
 
-public class UserProfilePage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_page);
+    }
 }
