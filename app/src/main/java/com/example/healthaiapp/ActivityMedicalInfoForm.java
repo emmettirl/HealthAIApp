5468fe591aad340e743d26b5473b850dbf6c0c59
Emmett Fitzharris <emmett.fitzharris@mycit.ie>
2023-10-24 20:52:51 +0100

    Non functional EOD commit

--- app/src/main/java/com/example/healthaiapp/ActivityMedicalInfoForm.java
+++ app/src/main/java/com/example/healthaiapp/ActivityMedicalInfoForm.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityMedialInfoForm extends AppCompatActivity {
+public class ActivityMedicalInfoForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medial_info_form);
+        setContentView(R.layout.activity_medical_info_form);
     }
 }
