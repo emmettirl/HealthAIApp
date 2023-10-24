54e043f1e838b4d16f2b5ed87e37e7fe97d4b2bd
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-10-24 19:15:47 +0100

    - Updated activity_landing_page.xml  - Finished basic layout  - Added AI Subscription and Support Quick Access Image Buttons  - Added localisation for the file
    Updated LandingPage.java
     - Added all the quick access buttons and functionality
     - Split onCreate into regions of Nav Buttons / Quick Access Buttons for neatness

--- app/src/main/java/com/example/healthaiapp/LandingPage.java
+++ app/src/main/java/com/example/healthaiapp/LandingPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LandingPage extends AppCompatActivity {
     @Override
@@ -12,6 +13,7 @@ public class LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
+        //region Nav Buttons
         Button userProfileButton = findViewById(R.id.userProfileNavButton);
         Button AIPredictPLACEHOLDER = findViewById(R.id.FitnessNavButton);
         Button FitnessPLACEHOLDER = findViewById(R.id.AIPredictNavButton);
@@ -19,7 +21,30 @@ public class LandingPage extends AppCompatActivity {
             Intent intent = new Intent(LandingPage.this, UserProfilePage.class);
             startActivity(intent);
         });
+        //endregion
 
+        //region Quick Access Buttons
+        ImageButton healthAIButtonPLACEHOLDER = findViewById(R.id.healthAIQuickAccessImageButton);
+        ImageButton ratingButton = findViewById(R.id.RatingQuickAccessImageButton);
+        ImageButton subscriptionButton = findViewById(R.id.subscriptionQuickAccessImageButton);
+        ImageButton supportButton = findViewById(R.id.supportQuickAccessImageButton);
+
+        ratingButton.setOnClickListener(view -> {
+            Intent intent = new Intent(LandingPage.this, RatingPage.class);
+            startActivity(intent);
+        });
+
+        subscriptionButton.setOnClickListener(view -> {
+            Intent intent = new Intent(LandingPage.this, SubscriptionPage.class);
+            startActivity(intent);
+        });
+
+        supportButton.setOnClickListener(view -> {
+            Intent intent = new Intent(LandingPage.this, SupportPage.class);
+            startActivity(intent);
+        });
+
+        //endregion
 
     }
 }
