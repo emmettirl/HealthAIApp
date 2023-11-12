159b82410fceea5ed70b8fbfb071e0cffce540b0
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-11-12 18:20:06 +0000

    - Added functionality to view all the reviews inside ReviewPage - Added UserProfilePage.java to AndroidManifest.xml - Tweaked view activity of review display

--- app/src/main/java/com/example/healthaiapp/RatingPage.java
+++ app/src/main/java/com/example/healthaiapp/RatingPage.java
@@ -4,11 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.healthaiapp.data.Review;
 import com.example.healthaiapp.data.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RatingPage  extends AppCompatActivity {
     private User loggedInUser;
+    private RecyclerView reviewsRecyclerView;
+    private ReviewAdapter reviewAdapter;
+    private List<Review> reviewList;
+    private DatabaseReference reviewsReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,7 @@ public class RatingPage  extends AppCompatActivity {
         });
         //endregion
 
+        //region Pass user info
         if (getIntent().hasExtra("loggedInUser")) {
             loggedInUser = (User) getIntent().getSerializableExtra("loggedInUser");
             Log.d("RatingPage", "Intent has 'loggedInUser' extra: " + getIntent().hasExtra("loggedInUser"));
@@ -42,5 +62,37 @@ public class RatingPage  extends AppCompatActivity {
         } else {
             Log.e("RatingPage", "User is not present in intent");
         }
+        //endregion
+
+        reviewsReference = FirebaseDatabase.getInstance().getReference().child("Reviews");
+        reviewsRecyclerView = findViewById(R.id.ratingRecyclerView);
+        reviewsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        reviewList = new ArrayList<>();
+        fetchReviewsFromFirebase();
+    }
+
+    private void fetchReviewsFromFirebase() {
+        reviewsReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                reviewList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Review review = snapshot.getValue(Review.class);
+                    if (review != null) {
+                        reviewList.add(review);
+                    }
+                }
+                updateRecyclerView();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("Database Retrieval", "Failed to retrieve data");
+            }
+        });
+    }
+
+    private void updateRecyclerView() {
+        reviewAdapter = new ReviewAdapter(reviewList);
+        reviewsRecyclerView.setAdapter(reviewAdapter);
     }
 }
---
+++ app/src/main/java/com/example/healthaiapp/ReviewAdapter.java
@@ -0,0 +1,55 @@
+package com.example.healthaiapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.healthaiapp.data.Review;
+
+import java.util.List;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
+    private List<Review> reviewList;
+
+    public ReviewAdapter(List<Review> reviews) {
+        this.reviewList = reviews;
+    }
+
+    @NonNull
+    @Override
+    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_item, parent, false);
+        return new ReviewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewViewHolder holder, int position) {
+        Review review = reviewList.get(position);
+
+        holder.titleTextView.setText(review.getTitle());
+        holder.descriptionTextView.setText(review.getContent());
+        holder.ratingBar.setRating(review.getRating());
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviewList.size();
+    }
+
+    public static class ReviewViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+        TextView descriptionTextView;
+        RatingBar ratingBar;
+
+        public ReviewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.ratingTitleDisplay);
+            descriptionTextView = itemView.findViewById(R.id.ratingContentDisplay);
+            ratingBar = itemView.findViewById(R.id.ratingBarDisplay);
+        }
+    }
+}
