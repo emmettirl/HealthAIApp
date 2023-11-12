b94a490d527432a8f4e3712c6ae29bb86c1fb238
PD-2001 <96789121+PD-2001@users.noreply.github.com>
2023-11-12 22:07:18 +0000

    - Added red text to display when the review is invalid

--- app/src/main/java/com/example/healthaiapp/LeaveRatingPage.java
+++ app/src/main/java/com/example/healthaiapp/LeaveRatingPage.java
@@ -3,8 +3,11 @@ package com.example.healthaiapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
+
 import androidx.lifecycle.ViewModelProvider;
 import com.example.healthaiapp.data.ReviewViewModel;
 import com.example.healthaiapp.data.User;
@@ -71,10 +74,13 @@ public class LeaveRatingPage extends AppCompatActivity {
 
             if (loggedInUser != null && isValidReview(rating, reviewTitle, reviewContent, userEmail)) {
                 reviewViewModel.addReview(rating, reviewTitle, reviewContent, userEmail);
-                // Show Success Page
+                // TO DO: Add page that will notify user of success
                 Log.e("RatingPage", "User Email: " + userEmail);
             } else {
-                // Handle invalid review or unavailability of loggedInUser
+                TextView errorTextView = findViewById(R.id.invalidReviewText);
+                TextView errorExplanationTextView = findViewById(R.id.invalidReviewExplanationText);
+                errorTextView.setVisibility(View.VISIBLE);
+                errorExplanationTextView.setVisibility(View.VISIBLE);
             }
         });
 
