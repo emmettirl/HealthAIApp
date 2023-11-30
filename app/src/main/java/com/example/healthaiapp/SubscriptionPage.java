bfd7b97985d56d5c09ac0ba702900746291ba7a8
Emmett <emmett.fitzharris@mycit.ie>
2023-11-30 17:58:15 +0000

    App now recognizes subscriptions

--- app/src/main/java/com/example/healthaiapp/SubscriptionPage.java
+++ app/src/main/java/com/example/healthaiapp/SubscriptionPage.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.healthaiapp.data.User;
+import com.example.healthaiapp.data.UserViewModel;
 import com.github.kittinunf.fuel.Fuel;
 import com.github.kittinunf.fuel.core.FuelError;
 import com.github.kittinunf.fuel.core.Handler;
@@ -19,18 +20,29 @@ import com.stripe.android.paymentsheet.PaymentSheetResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.nio.charset.StandardCharsets;
+
 public class SubscriptionPage extends AppCompatActivity {
 
+    UserViewModel uvm;
+
+
     private static final String TAG = "SubscriptionPage";
+    User loggedInUser;
+    String stripeID;
     PaymentSheet paymentSheet;
     private String paymentClientSecret;
     PaymentSheet.CustomerConfiguration customerConfig;
+    Boolean premiumCheck = Boolean.FALSE;
+    Button paymentPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription_page);
 
+        uvm = new UserViewModel();
         paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
 
         Intent thisIntent = getIntent();
@@ -38,26 +50,32 @@ public class SubscriptionPage extends AppCompatActivity {
             User loggedInUser = (User) thisIntent.getSerializableExtra("loggedInUser");
 
             assert loggedInUser != null;
-            String name = loggedInUser.getUsername();
+            this.loggedInUser = loggedInUser;
         }
 
         //region Nav Buttons
         Button userProfileButton = findViewById(R.id.userProfileNavButton);
         Button AIPredictPLACEHOLDER = findViewById(R.id.AIPredictNavButton);
         Button FitnessPageButton = findViewById(R.id.FitnessNavButton);
-        Button paymentPageButton = findViewById(R.id.SubscriptionPaymentButton);
+        this.paymentPageButton = findViewById(R.id.SubscriptionPaymentButton);
 
         userProfileButton.setOnClickListener(view -> {
             Intent intent = new Intent(SubscriptionPage.this, UserProfilePage.class);
             startActivity(intent);
         });
+
         FitnessPageButton.setOnClickListener(view -> {
             Intent intent = new Intent(SubscriptionPage.this, FitnessPage.class);
             startActivity(intent);
         });
 
+        this.stripeID = this.loggedInUser.getStripeID();
+        checkActiveSubscription(stripeID);
+
+
         paymentPageButton.setOnClickListener(view -> {
             Fuel.INSTANCE.post("http://10.0.2.2:4567/create-subscription", null).responseString(new Handler<String>() {
+
                 @Override
                 public void success(String s) {
                     try {
@@ -66,9 +84,9 @@ public class SubscriptionPage extends AppCompatActivity {
                                 result.getString("customer"),
                                 result.getString("ephemeralKey")
                         );
-                        // Change this to 'clientSecret' received from your server
-                        paymentClientSecret = result.getString("clientSecret");
 
+
+                        paymentClientSecret = result.getString("clientSecret");
                         PaymentConfiguration.init(getApplicationContext(), result.getString("publishableKey"));
                         presentPaymentSheet();
                         Log.d(TAG, "success: ");
@@ -84,23 +102,63 @@ public class SubscriptionPage extends AppCompatActivity {
             });
         });
     }
+
+    private void checkActiveSubscription(String stripeID) {
+        Fuel.INSTANCE.post("http://10.0.2.2:4567/check-subscription", null)
+                .body("customerId=" + stripeID, StandardCharsets.UTF_8)
+                .responseString(new Handler<String>() {
+                    @Override
+                    public void success(String s) {
+                        try {
+                            final JSONObject result = new JSONObject(s);
+                            boolean hasActiveSubscription = result.getBoolean("hasActiveSubscription");
+
+                            if (hasActiveSubscription) {
+                                premiumCheck = Boolean.TRUE;
+                                paymentPageButton.setText("Already Subscribed");
+                                paymentPageButton.setOnClickListener(null);
+
+                            }
+
+                        } catch (JSONException e) {
+                            Log.d(TAG, e.toString());
+                        }
+                    }
+
+                    @Override
+                    public void failure(@NonNull FuelError fuelError) {
+                        Log.d(TAG, "failure: " + fuelError);
+                    }
+                });
+    }
+
     private void presentPaymentSheet() {
+
         final PaymentSheet.Configuration configuration = new PaymentSheet.Configuration.Builder("Example, Inc.")
                 .customer(customerConfig)
                 .allowsDelayedPaymentMethods(true)
                 .build();
+
         paymentSheet.presentWithPaymentIntent(
                 paymentClientSecret,
                 configuration
         );
     }
-
     void onPaymentSheetResult(final PaymentSheetResult paymentSheetResult) {
+
         if (paymentSheetResult instanceof PaymentSheetResult.Canceled) {
             Log.d(TAG, "Canceled");
+
         } else if (paymentSheetResult instanceof PaymentSheetResult.Failed) {
             Log.e(TAG, "Got error: ", ((PaymentSheetResult.Failed) paymentSheetResult).getError());
+
         } else if (paymentSheetResult instanceof PaymentSheetResult.Completed) {
+            loggedInUser.setStripeID(customerConfig.getId());
+            loggedInUser.setPremium(Boolean.TRUE);
+            premiumCheck = Boolean.TRUE;
+            paymentPageButton.setText("Already Subscribed");
+            paymentPageButton.setOnClickListener(null);
+            uvm.updateUser(loggedInUser);
             Log.d(TAG, "Completed");
         }
     }
